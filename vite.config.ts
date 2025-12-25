3a5ae27af0947557eae01a118dbd61a15a301fee
kaodi-addis <addiskaodi@gmail.com>
2025-12-25 14:33:16 +0300

    Fix tsconfig issue on Deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // Since the user root is conceptually the project root
-  root: './',
+  plugins: [react()],
   define: {
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
-    'process.env': process.env
   },
   server: {
     port: 3000,
